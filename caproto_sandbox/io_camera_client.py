e1da7172fbb08f4bf51df031f8a323b257236613
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2019-10-08 14:25:44 -0400

    changed io_camera

--- caproto_sandbox/io_camera_client.py
+++ caproto_sandbox/io_camera_client.py
@@ -9,24 +9,29 @@ from time import time
 from caproto.threading.client import Context
 from pdb import pm
 default_prefix='camera:'
-def epics_client():
-    print('epics Client')
-    image = epics.PV(pvname = default_prefix+'image',connection_timeout = 20)
-    t1 = time()
-    img = image.get(timeout = 20)
-    t2 = time()
-    print(t2-t1, img.mean(), img.max(), img.min())
-    img2 = img.reshape((3960,3960))
-    print(t2-t1, img2.mean(), img2.max(), img2.min())
 
-def caproto_client():
-    print('caproto Client')
-    ctx = Context()
-    img_caproto, = ctx.get_pvs(default_prefix+'image')
-    t1 = time()
-    img_caproto_data = img_caproto.read()
-    t2 = time()
-    print(t2-t1, img_caproto_data.data.reshape((3960,3960)).mean(), img_caproto_data.data.reshape((3960,3960)).max(), img_caproto_data.data.reshape((3960,3960)).min())
+print('epics Client')
+image = epics.PV(pvname = default_prefix+'image', connection_timeout = 20)
+t1 = time()
+def image_get():
+    global image
+    return image.get(timeout = 20)
+img = image_get()
+t2 = time()
+print(t2-t1, img.mean(), img.max(), img.min())
+img2 = img.reshape((3960,3960))
 
-epics_client()
-caproto_client()
+print('caproto Client')
+ctx = Context()
+img_caproto, = ctx.get_pvs(default_prefix+'image')
+t1 = time()
+img_caproto_data = img_caproto.read()
+t2 = time()
+print(t2-t1, img_caproto_data.data.reshape((3960,3960)).mean(), img_caproto_data.data.reshape((3960,3960)).max(), img_caproto_data.data.reshape((3960,3960)).min())
+
+
+from timeit import timeit
+t = timeit(img_caproto.read, number = 10)
+print('caproto client: {}'.format(t/10))
+t = timeit(image_get, number = 10)
+print('pyepics client: {}'.format(t/10))
