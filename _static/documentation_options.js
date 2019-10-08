b2cb325842a0ef9f92de11f5ba8aa12cf60f9ebb
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-10-08 18:48:37 +0000

    Update docs after building Travis build 8 of vstadnytskyi/caproto-sandbox
    The docs were built from the branch 'master' against the commit
    c28ea49844a7151522e8c6be6651f435fb9fe471.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/vstadnytskyi/caproto-sandbox/jobs/595259680.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.post4+ga61e01f',
+    VERSION: '0.0.0.post5+gc28ea49',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
