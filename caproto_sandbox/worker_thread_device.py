b0d04a63561ae615ac226077413aa74587cd3cfe
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2020-02-04 18:35:29 -0600

    Threading Worker example

--- caproto_sandbox/worker_thread_device.py
+++ caproto_sandbox/worker_thread_device.py
@@ -7,18 +7,20 @@ from caproto.server import pvproperty, PVGroup, ioc_arg_parser, run
 
 class Device():
     def get_time(self, request_queue, response_queue):
-        from time import sleep, time
-        while True:
-            request = request_queue.get()
+        """
+        In this toy example, the "request" is some number of seconds to sleep for, but it could be any blocking task. For example, it can be a command to a device to perform some task.
 
-            # In this toy example, the "request" is some number of seconds to
-            # sleep for, but it could be any blocking task. For example, it can be a command to a device to perform some task.
-            print(f'performing the task of reading "position"')
-            sleep(0.2)
-            position = time()
-            # In this toy example, the "response" is just a counter of how many
-            # requests we have seen, but it could be anything.
-            response_queue.put(position)
+        In this example, we return current time after sleeping 0.2 s. This emulates communication to a device that takes time and return of the new value.
+        """
+
+        from time import sleep, time
+
+        print(f'performing the task of reading "position"')
+        sleep(0.2)
+        position = time()
+        # In this toy example, the "response" is just a counter of how many
+        # requests we have seen, but it could be anything.
+        response_queue.put(position)
 
 
 if __name__ == '__main__':
