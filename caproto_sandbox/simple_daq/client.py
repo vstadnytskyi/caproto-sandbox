09d04cca1b2524a5793f5e5f908ee50ae4ed17e2
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2021-10-13 10:38:22 -0400

    documentation

--- caproto_sandbox/simple_daq/client.py
+++ caproto_sandbox/simple_daq/client.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
-from caproto.threading.client import Context
-ctx = Context()
-dt,t,cpu,memory,battery = ctx.get_pvs('simple:dt','simple:TIME','simple:CPU','simple:MEMORY','simple:BATTERY')
+
+class Client(object):
+    def __init__(self):
+        from caproto.threading.client import Context
+        ctx = Context()
+        dt,t,cpu,memory,battery = ctx.get_pvs('simple_daq:dt','simple_daq:TIME','simple_daq:CPU','simple_daq:MEMORY','simple_daq:BATTERY')
+
+        self.dt,self.t,self.cpu,self.memory,self.battery  = dt,t,cpu,memory,battery
