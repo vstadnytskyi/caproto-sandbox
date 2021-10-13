4917359be0bc96e8c8523e6f76f58470a51a86c7
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2021-10-13 11:22:32 -0400

    docs

--- caproto_sandbox/simple_daq/client.py
+++ caproto_sandbox/simple_daq/client.py
@@ -7,3 +7,6 @@ class Client(object):
         dt,t,cpu,memory,battery = ctx.get_pvs('simple_daq:dt','simple_daq:TIME','simple_daq:CPU','simple_daq:MEMORY','simple_daq:BATTERY')
 
         self.dt,self.t,self.cpu,self.memory,self.battery  = dt,t,cpu,memory,battery
+
+if __name__ == '__main__':
+    client = Client()
