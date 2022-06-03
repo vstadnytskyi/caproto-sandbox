5aa669ec2976e5e0e2d66442311d1b75416f8278
Valentyn Stadnytskyi <v.stadnytskyi@gmail.com>
2022-06-03 16:23:34 -0400

    update

--- caproto_sandbox/transition/pcaspy/simple_daq_push_server.py
+++ caproto_sandbox/transition/pcaspy/simple_daq_push_server.py
@@ -45,7 +45,7 @@ pvdb = {
         'scan' : 1,
             },
     'dt' : {
-        'value': 0.0,
+        'value': 1.0,
         'prec' : 1,
         'scan' : 1,
         'count': 1,
@@ -57,12 +57,26 @@ pvdb = {
 class myDriver(Driver):
     def __init__(self):
         super(myDriver, self).__init__()
+        from time import time
+        self.t_start = time()
+
+    # def poll(self):
+    #     while True:
+    #         self.setParam('TIME', time.ctime())
+    #         self.setParam('CPU', psutil.cpu_percent())
+    #         if psutil.sensors_battery() is not None:
+    #             self.setParam('BATTERY', psutil.sensors_battery().percent)
+    #         self.setParam('MEMORY', psutil.virtual_memory().used / (1024**3))
+
+    #         self.updatePVs()
+
+    #         time.sleep(self.getParam('dt'))
 
     def read(self, reason):
         from time import ctime, time
         import psutil
         if reason == 'TIME':
-            value = ctime(time())
+            value = time()-self.t_start
         elif reason == 'CPU':
             value = psutil.cpu_percent()
         elif reason == 'BATTERY':
@@ -76,6 +90,13 @@ class myDriver(Driver):
             value = self.getParam(reason)
         return value
 
+    def write(self, reason, value):
+        from time import ctime, time
+        import psutil
+        if reason == 'dt':
+            print(f"dt was written and new dt is {value}")
+            self.setParam(reason,value)
+
 if __name__ == '__main__':
     from time import sleep,ctime, time
     server = SimpleServer()
@@ -83,4 +104,3 @@ if __name__ == '__main__':
     driver = myDriver()
     while True:
         server.process(driver.getParam('dt'))
-        print(ctime(time()))
