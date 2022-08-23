56281ae4cd19f3f3bf422168a6c5b4972a936187
vstadnytskyi <vstadnytskyi@users.noreply.github.com>
2022-08-23 17:03:40 +0000

    deploy: 506d0ad0f6f0f7b6f5d1f6cc52455198f9f88898

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.post84+g06f6b42',
+    VERSION: '0.0.0.post85+g506d0ad',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
